5b0b52f00164276ed617d1d22350f9c44d8c9a69
adamspruijt <adamspruijt@gmail.com>
2013-11-06 16:25:59 -0500

    Simplified to just default template for now

--- site/layouts/default.php
+++ site/layouts/default.php
@@ -2,18 +2,25 @@
 
 <div class="container">
 
-	 <h6><?php echo $page->date ?></h6>
+	<h6><?php echo $page->date ?></h6>
 	<hr>
 	<hr>
 	<?php echo $page->content ?>
-	<p>
+	
+	<?php if ($page->children): ?>
+	<div class="panel panel-default">
+		<div class="panel-body">
+			<h4>Child Pages</h4>
+			<ul>
+				<?php foreach ($page->children as $p): ?>
+					<li><a href="<?php echo $p->url ?>"><?php echo $p->title ?></a></li>
+				<?php endforeach ?>
+			</ul>
+		</div>
+	</div>	
+	<?php endif ?>
 
-		<ul>
-			<?php foreach ($page->children as $p): ?>
-				<li><a href="<?php echo $p->url ?>"><?php echo $p->title ?></a></li>
-			<?php endforeach ?>
-		</ul>
-	</p>
+	<?php $page->parent() ?>
 
 
 </div>
--- site/layouts/home.php
+++
@@ -1,6 +0,0 @@
-<?php include '_head.inc'; ?>
-<div class="container">
-	<h3>Date: <?php echo $page->date ?></h3>
-	<?php echo $page->content ?>
-</div>
-<?php include '_foot.inc'; ?>
