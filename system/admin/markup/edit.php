67c3bcfc224563d437cddbd6a05bc7e28b2e1b5f
Adam Spruijt <adamspruijt@gmail.com>
2013-11-08 19:43:37 -0500

    rough col use working in admin

--- system/admin/markup/edit.php
+++ system/admin/markup/edit.php
@@ -11,20 +11,37 @@
 	
 	<?php 
 
-	$rowCount = 0;
+	$colCount = 0;
 	foreach ($pageTemplate->_data as $key => $value): 
+		$attr = $value->attributes();
+		
 
 		$field = new Field($key);
 
 		?>
 		
-	<div class="panel panel-default">
-		<div class="panel-heading"><?php echo $field->label ?></div>
-		<div class="panel-body">
-			<input class="form-control" type="text" name="title" id="" value="<?php echo $pageEdit->$key ?>">
+	<?php if (!$colCount): ?>
+		<div class="row">
+	<?php endif ?>
+	<?php 
+		$colCount = $colCount+$attr->col;
+	 ?>
+	
+	<div class="col-md-<?php echo $attr->col ?>">
+		<div class="panel panel-default">
+			<div class="panel-heading"><?php echo $field->label ?></div>
+			<div class="panel-body">
+				<input class="form-control" type="text" name="title" id="" value="<?php echo $pageEdit->$key ?>">
+			</div>
 		</div>
 	</div>
-	<?php endforeach ?>
+	<?php 
+	if ($colCount == 12):
+	 $colCount = 0;?>
+	</div>
+<?php endif; ?>
+	<?php
+	endforeach; ?>
 
 
 </form>
