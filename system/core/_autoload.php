3dd80339199f26c2589c93077d7427bc432f3efe
Adam Spruijt <adamspruijt@gmail.com>
2013-11-30 21:10:59 -0500

    quick fixes

--- system/core/_autoload.php
+++ system/core/_autoload.php
@@ -14,15 +14,20 @@ function classLoader($className) {
     $className = str_replace("\\",DIRECTORY_SEPARATOR,$className);
 
     $systemPath = ROOT_PATH."system".DIRECTORY_SEPARATOR."core".DIRECTORY_SEPARATOR;
-    $fieldtypePath = ROOT_PATH."system".DIRECTORY_SEPARATOR."plugins".DIRECTORY_SEPARATOR."fieldtypes".DIRECTORY_SEPARATOR.$className.DIRECTORY_SEPARATOR;
+
     $namespacePath = empty($namespace) ? "" : $namespace.DIRECTORY_SEPARATOR;
 
     $coreClass = "{$systemPath}{$namespacePath}{$className}.php";
-    $fieldtype = "{$fieldtypePath}{$namespacePath}{$className}.php";
+
 
     // var_dump($fieldtype);
 
     if(is_file($coreClass)) require_once($coreClass);
-    elseif(is_file($fieldtype)) require_once($fieldtype);
+    else{
+        $fieldtypePath = ROOT_PATH."system".DIRECTORY_SEPARATOR."plugins".DIRECTORY_SEPARATOR."fieldtypes".DIRECTORY_SEPARATOR.$className.DIRECTORY_SEPARATOR;
+        $fieldtype = "{$fieldtypePath}{$namespacePath}{$className}.php"; 
+        if(is_file($fieldtype)) require_once($fieldtype);
+    }
+
 
 }
