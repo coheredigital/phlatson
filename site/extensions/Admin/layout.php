e9bab2f2349c0582f3d8e8bf07a9fa1e51a33904
Adam Spruijt <adamspruijt@gmail.com>
2015-02-19 20:52:48 -0500

    admin style updates to go with new branding

--- site/extensions/Admin/layout.php
+++ site/extensions/Admin/layout.php
@@ -10,61 +10,74 @@
     ?>
 </head>
 <body>
-    <div id="sidebar">
-        <div class="logo">
-            <img src="<?php echo $this->url ?>styles/images/logo.png" alt=""/>
+    <div id="header">
+        <div class="container">
+
+
+            <div class="logo">
+                <img src="<?php echo $this->url ?>styles/images/logo.png" alt=""/>
+            </div><div class="main-menu menu">
+
+                <?php
+                $menuLinks = [
+                  "Pages" => [
+                      "icon" => "file",
+                      "url" => $router->pages->url
+                  ],
+                  "Fields" => [
+                      "icon" => "edit",
+                      "url" => $router->fields->url
+                  ],
+                  "Templates" => [
+                      "icon" => "code",
+                      "url" => $router->templates->url
+                  ],
+                  "Extensions" => [
+                      "icon" => "cubes",
+                      "url" => $router->extensions->url
+                  ],
+                  "Users" => [
+                      "icon" => "users",
+                      "url" => $router->users->url
+                  ]
+                ];
+
+                ?>
+
+                <?php foreach ($menuLinks as $key => $value): ?>
+                    <?php $class = $value['url'] == $request->url ? "active" : "" ?>
+                    <a class="item <?php echo $class ?>" href="<?php echo $value['url'] ?>">
+                        <i class="icon icon-<?php echo $value['icon'] ?>"></i>
+                        <?php echo $key ?>
+                    </a>
+                <?php endforeach; ?>
+            </div>
+
         </div>
-        <div class="user-menu">
+
+    </div>
+    <div id="main">
+
+        <?php if ($this->title): ?>
+            <div class="main-title">
+                <div class="container">
+                    <?php echo $this->title ?>
+                </div>
+            </div>
+        <?php endif; ?>
+
+
+        <?php echo $this->output ?>
+    </div>
+    <div class="user-menu">
+        <div class="container">
             <div class="user-menu-content">
                 <div class="user-name"><?php echo $user->name ?></div><a href="<?php echo $config->urls->admin ?>logout" class="user-logout"><i class="icon icon-lock"></i> Logout</a>
             </div>
         </div>
 
-        <div class="ui menu vertical main-menu">
-
-            <?php
-            $menuLinks = [
-              "Pages" => [
-                  "icon" => "file",
-                  "url" => $router->pages->url
-              ],
-              "Fields" => [
-                  "icon" => "edit",
-                  "url" => $router->fields->url
-              ],
-              "Templates" => [
-                  "icon" => "code",
-                  "url" => $router->templates->url
-              ],
-              "Extensions" => [
-                  "icon" => "cubes",
-                  "url" => $router->extensions->url
-              ],
-              "Users" => [
-                  "icon" => "users",
-                  "url" => $router->users->url
-              ]
-            ];
-
-            ?>
-
-            <?php foreach ($menuLinks as $key => $value): ?>
-                <?php $class = $value['url'] == $request->url ? "active" : "" ?>
-                <a class="item <?php echo $class ?>" href="<?php echo $value['url'] ?>">
-                    <i class="icon icon-<?php echo $value['icon'] ?>"></i>
-                    <?php echo $key ?>
-                </a>
-            <?php endforeach; ?>
-        </div>
-
-    </div><div id="main">
-        <?php if ($this->title): ?>
-            <div class="main-title">
-                <?php echo $this->title ?>
-            </div>
-        <?php endif; ?>
-        <?php echo $this->output ?>
-    </div><?php
+    </div>
+    <?php
 foreach ($config->scripts as $file) {
     echo "\n    <script src='{$file}'></script>";
 }?>
