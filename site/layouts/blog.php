debef181e5a83a311419db77a6d676384d79eee4
Adam Spruijt <adamspruijt@gmail.com>
2015-12-11 16:27:38 -0500

    tweaks to front end test site

--- site/layouts/blog.php
+++ site/layouts/blog.php
@@ -4,11 +4,21 @@
         <?php
 
         $children = $page->children;
+        $children->sort("modified");
+
 
         if ($children): ?>
-            <?php foreach ($children as $p): ?>
-                <h3><a href="<?php echo $p->url ?>"><?php echo $p->title ?></a></h3>
-                <p><?php echo $p->content ?></p>
+            <?php foreach ($children as $p):?>
+                <article>
+                    <header>
+                        <h3 class="title"><a href="<?php echo $p->url ?>"><?php echo $p->title ?></a></h3>
+                        <h6><?php echo $p->modified ?></h6>
+                    </header>
+
+
+                    <?php echo $p->content ?>
+                </article>
+
             <?php endforeach ?>
         <?php endif ?>
     </div>
