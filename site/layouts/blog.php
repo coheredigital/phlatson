98f0bf2457e76175b969e26d656049a6ffc96db7
Adam Spruijt <adamspruijt@gmail.com>
2015-12-11 11:36:47 -0500

    testing sort

--- site/layouts/blog.php
+++ site/layouts/blog.php
@@ -1,8 +1,12 @@
 <?php include 'includes/head.inc' ?>
     <div class="container">
         <!-- PAGE CONTENT -->
-        <?php if ($page->children): ?>
-            <?php foreach ($page->children as $p): ?>
+        <?php
+
+        $children = $page->children;
+
+        if ($children): ?>
+            <?php foreach ($children as $p): ?>
                 <h3><a href="<?php echo $p->url ?>"><?php echo $p->title ?></a></h3>
                 <p><?php echo $p->content ?></p>
             <?php endforeach ?>
