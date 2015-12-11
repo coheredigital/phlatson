7bd710b6918aea01ae6802e2c625564ef84caaef
Adam Spruijt <adamspruijt@gmail.com>
2015-12-11 16:24:41 -0500

    Added InputHidden to system, was missing, woops

---
+++ system/extensions/InputHidden/InputHidden.php
@@ -0,0 +1,22 @@
+<?php
+
+class InputHidden extends Input {
+
+    public function setup()
+    {
+        $this->api('config')->styles->add("{$this->url}{$this->name}.css");
+    }
+
+    protected function renderInput()
+    {
+        $this->attribute("type", "hidden");
+
+        if ($this->value) {
+            $this->attribute("value", $this->value);
+        }
+
+        $output = "<input {$this->attributes}>";
+        return $output;
+    }
+
+}
