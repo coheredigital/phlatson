fb8e6c68b12229ea4a3d67a8a1ce05dd7c0cba0c
Adam Spruijt <adamspruijt@gmail.com>
2017-03-22 21:29:36 -0400

    switched to using Views class

--- system/extensions/FieldtypeView/FieldtypeView.php
+++ system/extensions/FieldtypeView/FieldtypeView.php
@@ -6,10 +6,9 @@ class FieldtypeView extends Fieldtype
 	public function getOutput($value)
     {
         
-    	$file = $this->api('config')->paths->views . $value . ".php";
-
-    	if (!file_exists($file)) return false;
-	    return $file;
+        $file = $this->api('views')->get($value);
+        if (file_exists($file)) return $file;
+        return '';
     }
 
 	public function getEdit($value)
