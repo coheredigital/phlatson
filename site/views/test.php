7e8a898e32310d43d104c6bef830cbad39aca8cd
Adam Spruijt <adamspruijt@gmail.com>
2017-03-30 19:47:34 -0400

    test update

--- site/views/test.php
+++ site/views/test.php
@@ -8,17 +8,16 @@ ref::config('validHtml', true);
 $input = $extensions->get("InputTinymce");
 
 r($input->file);
+r($input->getUnformatted('path'));
 r($input->path);
 r($input->getPath());
 r($input->url);
+
 ?>
-<hr>
-<hr>
-<hr>
-<hr>
-<?php 
+<hr><hr><hr><hr>
+<?php
+
 r($page->file);
-r($page->parent);
 r($page->path);
 r($page->getPath());
 r($page->url);
@@ -27,14 +26,10 @@ r($page->url);
 <hr>
 <hr>
 <hr>
-<?php 
+<?php
 
 $field = $fields->get('title');
 r($field->file);
 r($field->path);
 r($field->getPath());
 r($field->url);
-
-
-
-
