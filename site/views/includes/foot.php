fcbc25089bf1e24048f196d48edc8fbf5093af11
Adam Spruijt <adamspruijt@gmail.com>
2017-03-31 18:57:33 -0400

    setup to work on Exceptions

--- site/views/includes/foot.php
+++ site/views/includes/foot.php
@@ -1,6 +1,7 @@
 </div>
 <div class="footer">
     <p>Flatbed Demo Site &copy; <?php echo date('Y'); ?></p>
+    <?php echo r($page) ?>
 </div>
 <div class="container">
 <br>
--- site/views/includes/head.php
+++ site/views/includes/head.php
@@ -1,3 +1,7 @@
+<?php // add ref for debugging, remove later
+require ROOT_PATH . "libraries/ref/ref.php";
+ref::config('expLvl', 1);
+ref::config('validHtml', true); ?>
 <!doctype html>
 <html lang="en">
 <head>
