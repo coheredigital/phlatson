bae4133235b3133fbb1d7372c5320b9644d80e8e
Adam Spruijt <adamspruijt@gmail.com>
2017-04-01 12:35:04 -0400

    improved handling of default datetime format

--- system/core/FlatbedDateTime.php
+++ system/core/FlatbedDateTime.php
@@ -8,12 +8,14 @@
  *
  * Added __toString magic method and $outputFormat variable / setter to facilitate
  *
+ *  TODO: reference Drupal DateTimePlus for some ideas on how to extend
+ *
  */
 
 class FlatbedDateTime extends DateTime
 {
 
-    protected $outputFormat = "F j, Y";
+    protected $outputFormat = "c"; // default to ISO 8601
 
     public function setOutputFormat($format){
         $this->outputFormat = $format;
@@ -21,8 +23,18 @@ class FlatbedDateTime extends DateTime
 
     public function __toString()
     {
-
-        return $this->format($this->outputFormat);
+        return parent::format($this->outputFormat);
     }
+
+    /**
+     * allow DateTime format to use the FlatbedDateTime default
+     * @param  string $format DateTime compatible format
+     * @return string         DateTime in requested format
+     */
+    public function format($format)
+    {
+        return parent::format( $format ?? $this->outputFormat );
+    }
+
 
 }
