0f806f3d787959c72a2e675e71ddb9d346256da1
Adam Spruijt <adamspruijt@gmail.com>
2017-04-13 20:45:41 -0400

    building on router to support matching pages that support URL segments

--- system/core/Router.php
+++ system/core/Router.php
@@ -1,6 +1,6 @@
 <?php
 
-class Router
+class Router extends Flatbed
 {
 
     protected $request;
@@ -10,5 +10,50 @@ class Router
     {
         $this->request = $request;
     }
+
+
+    protected function match($path) {
+
+    	// check for an exact page match
+	    foreach ($this->request->urls as $key => $url) {
+	    	
+	    	$page = $this('pages')->get($url);
+
+	    	
+	    	if ($page) {
+	    		// page match on first url is an exact match, return it
+	    		if ($key === 0) {
+	    			$this->request->setMatch($key);
+	    			return $page;
+	    		}
+	    		// subsequent match is a partial match, and requires template to support url segments
+	    		else if($page->template->setting('urlSegments') ) {
+	    			$this->request->setMatch($key);
+	    			return $page;
+	    		}
+	    		// otherwise we stop checking
+	    		else break;
+	    	}
+
+	    }
+
+    }
+
+
+    public function execute() {
+		
+
+    	$match = $this->match($this->request->path);
+
+    	if ($match) {
+    		return $match->render();
+    	}
+	    
+        // TODO :  I'd like to see if I can do this without the need for a page and template
+        return $this('pages')->get('404')->render();
+	    
+    
+    }
+
 
 }
