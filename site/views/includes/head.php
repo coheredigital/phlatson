4d3e89c1319d42fe214d92af1ac41a83730f4385
Adam Spruijt <adamspruijt@gmail.com>
2017-04-17 00:18:52 -0400

    added favicons
    currently just added to client site since admin is in the works

--- site/views/includes/head.php
+++ site/views/includes/head.php
@@ -10,6 +10,14 @@ ref::config('validHtml', true); ?>
     <link href='http://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Montserrat|Open+Sans' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="<?php echo $config->urls->views ?>styles/main.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/favicons/manifest.json">
+    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#f46b4b">
+    <link rel="shortcut icon" href="/favicons/favicon.ico">
+    <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body>
 <?php $home = $pages->get("/") ?>
