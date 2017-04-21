0fc49dabf4bb5276afa709993028a1dae23944ef
Adam Spruijt <adamspruijt@gmail.com>
2017-04-20 23:08:25 -0400

    trying out datatypes

---
+++ system/core/DatatypeInterger.php
@@ -0,0 +1,26 @@
+<?php
+
+class DatatypeInterger extends Datatype
+{
+
+    protected $value;
+
+    /**
+     * format value for output
+     *
+     * @param mixed raw input value
+     * @return mixed formatted publc facing variable
+     */
+    public function get($value) : int
+    {
+        
+        return $this->value;
+    }
+
+    public function set($value)
+    {
+        $this->value;
+    }
+
+
+}
