56de4ed124eebef252ef330b3cf417494358d030
Adam Spruijt <adamspruijt@gmail.com>
2017-04-23 16:26:51 -0400

    updated to support new controller format

--- system/controllers/admin.php
+++ system/controllers/admin.php
@@ -6,30 +6,25 @@
 // and the automatic action to take when user has no access
 // in this case, redirect to "login"
 
-if($user->isLoggedin() && $response->segment(1) === "logout") {
-	$session->logout();
-	$response->redirect("{$page->url}login/");
+if($this->user->isLoggedin() && $this->response->segment(1) === "logout") {
+	$this->session->logout();
+	$this->response->redirect("{$this->page->url}login/");
 }
 
-
-if ($user->isGuest() && $response->segment(1) != "login") {
-	$response->redirect("{$page->url}login/");
+if ($this->user->isGuest() && $this->response->segment(1) != "login") {
+	$this->response->redirect("{$this->page->url}login/");
 }
-if($user->isLoggedin() && $response->segment(1) === "login") {
-	$response->redirect($page->url);
+if($this->user->isLoggedin() && $this->response->segment(1) === "login") {
+	$this->response->redirect($this->page->url);
 }
 
-$page->layout = $views->get('layouts/default');
+$this->page->layout = $this->views->get('layouts/default');
 
 // login page is requested
-if ($response->segment(1) === "login") {
-
-
-
-
-	$page->layout = $views->get('layouts/login');
-	// $page->template->set('view', "admin.login");
+if ($this->response->segment(1) === "login") {
+	$this->page->layout = $this->views->get('layouts/login');
+	// $this->page->template->set('view', "admin.login");
 }
 else {
-	$page->layout->main .= $views->render('partials/header');
+	$this->page->layout->main .= $this->views->render('partials/header');
 }
