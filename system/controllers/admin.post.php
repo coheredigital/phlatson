8c71084ce4d6a1ae0373e261fb9c6ec2191f73e2
Adam Spruijt <adamspruijt@gmail.com>
2017-04-24 21:49:04 -0400

    fixed login to work with new controllers

--- system/controllers/admin.post.php
+++ system/controllers/admin.post.php
@@ -1,12 +1,12 @@
 <?php 
 
 // check for submitted form
-if ($request->post->username && $request->post->password) {
+if ($this->request->post->username && $this->request->post->password) {
     
-    $username = $request->post->username;
-    $password = $request->post->password;
+    $username = $this->request->post->username;
+    $password = $this->request->post->password;
 
-    if ($session->login($username, $password)) {
+    if ($this->session->login($username, $password)) {
         $response->redirect("$page->url/pages");
     }
 
