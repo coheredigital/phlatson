4efefb58ef037697e2817ac8785dc30cee62e233
Adam Spruijt <adamspruijt@gmail.com>
2017-04-30 00:02:58 -0400

    removed reference to named segements

--- Flatbed/Controller.php
+++ Flatbed/Controller.php
@@ -2,7 +2,6 @@
 namespace Flatbed;
 
 /*
-
 Controller loaded automatically based on the matching template
     example: Template: article loads Controller: /contollers/article.php
     or method spcific if defined: /contollers/article.post.php
@@ -10,18 +9,20 @@ Controller loaded automatically based on the matching template
 
 Controller is primarily defined to controller to use of construct, set to final to prevent overriding behavious
 and is devoid of method so that they can be bound at runtime since Controller extends Flatbed and is bind methods and API access
-
 */
 
 class Controller extends Flatbed
 {
 
     public $response;
-
     protected $routes;
 
     final public function __construct(Response $response)
     {
+
+        // create the routes collection
+        $this->routes = new RouteCollection;
+
         // TODO :  this should not be needed here, temp fix
         $this->response = $response;
 
@@ -29,6 +30,7 @@ class Controller extends Flatbed
         $name = $response->template->name;
 		$method = $this->request->method;
 
+        // determine root path based on isSystem() return value
 		if ($response->template->isSystem()) {
 			$path = SYSTEM_PATH . "controllers" . DIRECTORY_SEPARATOR;
 		}
@@ -47,31 +49,55 @@ class Controller extends Flatbed
         // no controller file was found, return
         if (!is_file($file)) return;
 
-        // extract named segment variables
-        if ($segments = $response->segments(true)) {
-            extract($segments);
-        }
-
         // and include controller
         include_once $file;
     }
 
-    public function respond($method, $path = '*', $callback = null)
+    public function respond($path = '*', Callable  $callback = null)
     {
-        // Get the arguments in a very loose format
-        extract(
-            $this->parseLooseArgumentOrder(func_get_args()),
-            EXTR_OVERWRITE
-        );
-        $route = $this->route_factory->build($callback, $path, $method);
-        $this->routes->add($route);
+
+        $method = 'GET';
+
+        if(strpos($path, ':') !== false) {
+            
+            list($method, $path) = explode(":",$path); 
+
+        }
+
+        // prepend current page to path
+        $path = rtrim($this->response->page->url, "/") . $path;
+
+        // $route = $this->route_factory->build($callback, $path, $method);
+        $route = new Route([
+            "method" => $method,
+            "path" => $path,
+            "callback" => $callback
+        ]);
+        $this->routes->append($route);
         return $route;
     }
 
-    public function respond(string $path, Callable $callback) {
+    /**
+     * @param Request $request
+     * @throws FlatbedException
+     *
+     */
+    public function run(Request $request)
+    {
+        $found = false;
 
+        foreach ($this->routes as $route) {
+            if (!$route->match($request)) continue;
+            $route->execute();
+            $found = true;
+            break;
+        }
+
+        if (!$found) {
+            d("route no matchy");
+        }
+        
     }
-
 
     // give property access to variable
     public function __get($name) {
