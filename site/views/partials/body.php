64c1e865291e72c40da679d9ce817552c3428f01
adamspruijt <adamspruijt@gmail.com>
2018-07-09 23:21:08 -0400

    remove debug fromdefault pages

--- site/views/partials/body.php
+++ site/views/partials/body.php
@@ -3,35 +3,10 @@
 		<?= $page->content ?>
 		<hr>
 	<?php endif; ?>
-	
-	<pre><table>
-		<tr>
-			<th>$page->name</th>
-			<td><?= $page->name ?></td>
-		</tr>
-		<tr>
-			<th>$page->url</th>
-			<td><?= $page->url ?></td>
-		</tr>
-		<tr>
-			<th>$page->folder</th>
-			<td><?= $page->folder ?></td>
-		</tr>
-		<tr>
-			<th>$page->path</th>
-			<td><?= $page->path ?></td>
-		</tr>
-		<tr>
-			<th>$page->file</th>
-			<td><?= $page->file ?></td>
-		</tr>
-		<tr>
-			<th>$page->filename</th>
-			<td><?= $page->filename ?></td>
-		</tr>
-
-	</table>
-	</pre>
+	<?php if ($page->markdown) : ?>
+		<?= $page->markdown ?>
+		<hr>
+	<?php endif; ?>
 	<?php if ($page->children()->count()): ?>
 	<h4>Children</h4>
 	<ul>
