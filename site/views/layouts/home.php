9299a8f900fa127d7f0f527c7150612d55d66157
adamspruijt <adamspruijt@gmail.com>
2018-07-09 20:17:49 -0400

    style cleanup

--- site/views/layouts/home.php
+++ site/views/layouts/home.php
@@ -13,31 +13,12 @@ $debugbarRenderer = $debugbar->getJavascriptRenderer();
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $page->title ?> | Phlatson</title>
     <link rel="stylesheet" href="<?= $view->rootUrl ?>styles/main.css?<?= date('U') ?>">
+    <link rel="stylesheet" href="<?= $view->rootUrl ?>styles/templates/home.css?<?= date('U') ?>">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:700,900">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono">
 </head>
 <body>
-    <div class="header">
-        <div class="container">
-            <div class="breadcrumbs">
-            <?php foreach ($page->parents() as $p) : ?>
-                <a href="<?= $p->url ?>"><?= $p->title ?></a> /
-            <?php endforeach; ?>
-            </div>
-            <h1><?= $page->get('title') ?></h1>
-        </div>
-        <div class="container">
-            <div class="menu">
-            <?php foreach ($home->children() as $p) : ?>
-                <a class="item" href="<?= $p->url ?>"><?= $p->title ?></a>
-            <?php endforeach; ?>
-            </div>
-		</div>
-    </div>
 	<?= $output ?>
-    </div>
-    <?= $debugbarRenderer->renderHead() ?>
-    <?= $debugbarRenderer->render() ?>
 </body>
 </html>
---
+++ site/views/partials/home/body.php
@@ -0,0 +1,29 @@
+<div class="page-header">
+	<div class="container">
+		<h1>Phlatson <span>CMS</span></h1>
+		<h5>The PHP Flat-file JSON CMS</h5>
+	</div>
+</div>
+<div class="feature">
+	<div class="container">
+		<div class="grid">
+			<div class="column -dektop-6">
+				<h4>JSON Data</h4>
+		<p>Vestibulum feugiat diam a eu parturient duis dis a sem netus fringilla condimentum tellus vestibulum risus adipiscing suspendisse ullamcorper consequat aliquam ullamcorper consectetur malesuada a maecenas id vestibulum dictum.</p>
+			</div>
+			<div class="column -dektop-6"></div>
+		</div>
+	<div class="content">
+		
+	</div>
+	<div class="code">
+		<pre>
+{
+  "title": "Canada",
+  "content": "Canada is a country located in the northern part of North America. Its ten provinces and three territories extend from the Atlantic to the Pacific",
+  "template": "home"
+}</pre>
+	</div>
+
+	</div>
+</div>
