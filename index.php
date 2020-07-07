0df5aedd9de56108b8a1420fb076d700abca24e3
Adam Spruijt <adamspruijt@gmail.com>
2020-07-07 15:51:16 -0400

    adding clockwork

--- index.php
+++ index.php
@@ -18,26 +18,31 @@ $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
+
+
 try {
 
     $phlatson = new Phlatson();
 
+    // TODO: move this to seperate init file / or addon
+    $clockwork = \Clockwork\Support\Vanilla\Clockwork::init([
+        'storage_files_path' => __DIR__ . "/storage/clockwork/"
+    ]);
+    $phlatson->api('clockwork',$clockwork);
+
     $finder = new Finder(__DIR__);
-    
     $finder->addPathMapping("Page", "/site/pages/");
     $finder->addPathMapping("Template", "/site/templates/");
     $finder->addPathMapping("Field", "/site/fields/");
     $finder->addPathMapping("Field", "/Phlatson/data/fields/");
     $finder->addPathMapping("Config", '/Phlatson/data/config/');
     $finder->addPathMapping("Config", '/site/config/');
-
-    // $finder->addPathMapping("Extension", '/site/extensions/');
-    // $finder->addPathMapping("Extension", '/Phlatson/data/extensions/');
     $phlatson->api("finder", $finder);
 
     $request = new Request();
-    
     $phlatson->api("request", $request);
+
+
 
     // determine the requested page
     $url = $request->url;
@@ -53,6 +58,8 @@ try {
     if ($view instanceof View) {
         echo $view->render();
     }
+
+    $clockwork->requestProcessed();
     
 } catch (\Exception $exception) {
     echo $exception;
--- site/views/default.php
+++ site/views/default.php
@@ -3,13 +3,12 @@
 namespace Phlatson;
 
 // FIXME: This crashes
-dump($request);
-dump($finder);
-dump($page);
+// \Kint::dump($request);
+// \Kint::dump($finder);
+// \Kint::dump($page);
 
 
 $test = $finder->getType("Page","/");
-dump($test->modified);
 
 echo $view->render('/layouts/default', [
     "output" => $view->render("/partials/body")
