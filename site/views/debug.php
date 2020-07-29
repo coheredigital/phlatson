7f71db7019f626d594948ddd6495bded485a0d86
Adam Spruijt <adamspruijt@gmail.com>
2020-07-29 19:05:22 -0400

    config setup

--- site/views/debug.php
+++ site/views/debug.php
@@ -2,11 +2,4 @@
 
 namespace Phlatson;
 
-
-dump($config);
-dump($page->parent());
-dump($page->url());
-dump($page->rootFolder());
-dump($page->template);
-dump($page->template());
-dump($page->template()->template());
+dump($page);
